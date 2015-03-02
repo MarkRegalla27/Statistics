a95fa3c1c5461d848485919a0b476ecd2ec75453
Mark Regalla <regalla.mark@gmail.com>
2015-03-01 22:32:09 -0500

    add changes to linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -1,4 +1,20 @@
 import pandas as pd
+
 loansData = pd.read_csv('https://spark-public.s3.amazonaws.com/dataanalysis/loansData.csv')
-#print loansData['Interest.Rate'][0:5]
-print loansData[0:0]
+
+percent = loansData['Interest.Rate'].map(lambda percent: round(float(percent.rstrip('%')),4))
+#print percent[0:5]
+
+#print loansData['Loan.Length'][0:5]
+
+term = loansData['Loan.Length'].map(lambda term: term.rstrip(' months'))
+#print term[0:5]
+
+cleanFICORange = loansData['FICO.Range'].map(lambda cleanFICORange: cleanFICORange.split('-'))
+cleanFICORange = cleanFICORange.map(lambda cleanFICORange: [int(n) for n in cleanFICORange])
+print cleanFICORange[0:5]
+print cleanFICORange[0:5].values[0][1]
+
+
+
+#g = lambda x: 
