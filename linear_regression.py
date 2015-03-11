b8fe6366e61bc663b9aa20127eab00cc9cddfa8d
Mark Regalla <regalla.mark@gmail.com>
2015-03-10 22:03:35 -0400

    Linear_regression.py FICO.Min as integer now functioning

--- linear_regression.py
+++ linear_regression.py
@@ -6,12 +6,15 @@ import numpy as np
 
 loansData = pd.read_csv('https://spark-public.s3.amazonaws.com/dataanalysis/loansData.csv')
 
-percent = loansData['Interest.Rate'].map(lambda percent: round(float(percent.rstrip('%')),4))
+loansData['Interest.Rate'] = loansData['Interest.Rate'].map(lambda percent: round(float(percent.rstrip('%')),4))
 #print percent[0:5]
 
 #print loansData['Loan.Length'][0:5]
 
-term = loansData['Loan.Length'].map(lambda term: int(term.rstrip(' months')))
+loansData['Loan.Length'] = loansData['Loan.Length'].map(lambda term: int(term.rstrip(' months')))
+print loansData.head()
+print loansData.dtypes
+
 #print term[0:5]
 #print type(term[0:5].values[0][0])
 
@@ -26,15 +29,16 @@ term = loansData['Loan.Length'].map(lambda term: int(term.rstrip(' months')))
 #cleanFICORange.hist(column=cleanFICORange)
 #plt.show()
 
-loansData['FICO.Min'] = loansData.apply(lambda x: x['FICO.Range'].split('-')[0:1], axis=1)
-loansData['FICO.Min'] = loansData['FICO.Min'].map(lambda y: [int(n) for n in y])
+loansData['FICO.Min'] = loansData.apply(lambda x: pd.Series(x['FICO.Range'].split('-')[0:1]).astype('int'), axis=1)
+#Series is an indexed 1 dimensional list in pandas, like an array in other languages
+#loansData['FICO.Min'] = loansData['FICO.Min'].map(lambda y: [int(n) for n in y])
 #print "FICO.Min"
 print loansData['FICO.Min'][0:5]
-#print type(loansData['FICO.Min'][0:5].values[0][0])
+#print type(loansData['FICO.Min'][0:5].values[0][0])	#values[row index][column index]
 
-plt.figure()
-loansData.hist(column='FICO.Min')
-plt.show()
+#plt.figure()
+#loansData.hist(column='FICO.Min')
+#plt.show()
 #i = 0
 #for i in cleanFICORange:
 #	loansData['FICO.Score'] = cleanFICORange.values[i][0]
@@ -42,8 +46,8 @@ plt.show()
 #loansData['FICO.Score'] = cleanFICORange
 #print loansData['FICO.Score'][0:5]
 #
-#scatter_matrix(loansData, alpha=0.05, figsize=(10,10))
-#plt.show()
+scatter_matrix(loansData, alpha=0.05, figsize=(10,10))  #prints only numerical values in dataframe, not objects/strings
+plt.show()
 
 #from notebook
 #matplotlib inline
