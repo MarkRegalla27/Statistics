e5b2f01b7ac9794fd18585bbacefb63689fd521b
Mark Regalla <regalla.mark@gmail.com>
2015-03-23 13:47:49 -0400

    commit 3-23-15 (linear_regression.py working better)

--- logistic_regression.py
+++ logistic_regression.py
@@ -9,7 +9,10 @@ loansData = pd.read_csv('https://spark-public.s3.amazonaws.com/dataanalysis/loan
 
 #Remove the "%" symbol from the interest rate data
 loansData['Interest.Rate'] = loansData['Interest.Rate'].map(lambda percent: round(float(percent.rstrip('%')),4))
-#print percent[0:5]
+i = 1
+print loansData['Interest.Rate'][:i]
+if loansData['Interest.Rate'][:i] > 12:
+	print 'yes'
 
 #Remove the word "months" from the Loan Length data
 loansData['Loan.Length'] = loansData['Loan.Length'].map(lambda term: int(term.rstrip(' months')))
@@ -18,17 +21,20 @@ loansData['Loan.Length'] = loansData['Loan.Length'].map(lambda term: int(term.rs
 loansData['FICO.Min'] = loansData.apply(lambda x: pd.Series(x['FICO.Range'].split('-')[0:1]).astype('int'), axis=1)
 
 #Add column indicating whether interest rate is greater than 12%
-#loansData['Less.Than.12'] = loansData['Interest.Rate'].map(lambda x: x['Less.Than.12'] = True if x['Interest.Rate'] < 12)
-#i = 0
-#for i in loansData:
-#	if loansData['Interest.Rate'][i] < 12:
-#		loansData['Greater.Than.12'][i] = True
-#	else:
-#		loansData['Greater.Than.12'][i] = False
-
+#loansData['Less.Than.12'] = [loansData['Less.Than.12'].bool() for line in loansData['Interest.Rate'] if loansData['Interest.Rate'] < 12]
+'''i = 0
+for i in loansData:
+	if [loansData['Interest.Rate'][0:i] < 12]:
+		loansData['Less.Than.12'][0:i] = True
+	else:
+		loansData['Less.Than.12'][0:i] = False
+'''
 #df[df['Interest.Rate'] == 10].head() # should all be True
 #df[df['Interest.Rate'] == 13].head() # should all be False
 #loansData['Greater.Than.12']= loansData['Interest.Rate'].map(lambda x: if loansData['Interest.rate'] < 12: loansData['Greater.Than.12'] = True 
 
 loansData['Intercept'] = 1.0
-print loansData['Intercept'][0:5]
+#print loansData['Intercept'][0:5]
+
+#Website on functional programming:
+#https://docs.python.org/2/howto/functional.html
