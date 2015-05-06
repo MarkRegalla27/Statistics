019fe08d07c133cafdd734689a4610f765f685f7
Mark Regalla <regalla.mark@gmail.com>
2015-05-05 21:15:27 -0400

    add time_series.py on 5-5-15

---
+++ time_series.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from pandas.tools.plotting import scatter_matrix
+from scipy import stats
+import numpy as np
+import statsmodels.api as sm
+from sklearn.cross_validation import train_test_split
+
+#loansData = pd.read_csv('LoanStats3c.csv')  #skiprows argument skips indicated row
+#loansData = pd.read_csv('/users/markregalla/projects/LoanStats3b.csv', skiprows=1)
+loansData = pd.read_csv('/users/markregalla/projects/LoanStats3b.csv', header=1, low_memory=False)
+#loansData = loansData.drop('desc', axis=1)
+
+# convert string to datetime object in pandas
+loansData['issue_d_format'] = pd.to_datetime(loansData['issue_d']) 
+dfts = loansData.set_index('issue_d_format') 
+year_month_summary = dfts.groupby(lambda x : x.year * 100 + x.month).count()
+loan_count_summary = year_month_summary['issue_d']
+
+column_names = loansData.columns.tolist()
+column_names = np.matrix(column_names).transpose()
+#print column_names
+#print year_month_summary	#24 rows x 52 columns
+print loan_count_summary
+#print dfts					#188125 rows x 52 columns
+#print loansData['issue_d_format']
+
+'''
+X = np.matrix(year_month_summary).transpose()
+Y = np.matrix(loan_count_summary).transpose()
+X = sm.add_constant(X)
+model = sm.OLS(Y,X)
+f = model.fit()
+'''
+
+plt.figure()
+#plt.plot(loan_count_summary, year_month_summary, 'r.')
+plt.plot(loan_count_summary, 'r.')
+plt.show()
+
+#print f.summary()
+
+
